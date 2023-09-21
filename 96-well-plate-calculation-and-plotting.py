323c29e187cd213eb48364141d9bb34261710b30
MaKaefer <32200167+MaKaefer@users.noreply.github.com>
2023-09-21 09:16:39 +0100

    plotting multiple plots during one run

--- 96-well-plate-calculation-and-plotting.py
+++ 96-well-plate-calculation-and-plotting.py
@@ -13,7 +13,7 @@ df = pd.read_csv('Z:/private/Marie/Image Analysis/2023-08-15-Zen-practice/FD-FD-
 df_column_names = list(df.columns.values)
 
 
-#rename columns
+#extract strings to rename columns to and extract data types
 def rename_column_headers(df_column_names):
     print("Renaming column headers to something you will recognise :) ")
     data_type = []
@@ -40,13 +40,14 @@ def rename_column_headers(df_column_names):
     return (new_column_names, data_type)
 
 new_column_names, data_type = rename_column_headers(df_column_names)
-print(new_column_names)
-print(data_type)
+#print(new_column_names)
+#print(data_type)
 
 
 #skip first row - contains column variable unit, consider to integrate into column names??
 df = df.iloc[2:,]
 
+#rename column names and set data type of columns
 i = 0
 while i < len(df_column_names):
     df = df.astype({df_column_names[i]:data_type[i]})
@@ -54,20 +55,21 @@ while i < len(df_column_names):
     i += 1
 del i
 
-"""
+
+
 pivot = np.round(pd.pivot_table(df, values='Convexity', 
-                                index='Time Index', 
-                                columns='Well', 
+                                index='Image Index Time', 
+                                columns='Image Scene Container Name ', 
                                 aggfunc=np.mean),3)
 
-pivot_count = np.round(pd.pivot_table(df, values='Convexity', 
-                                index='Time Index', 
-                                columns='Well', 
+pivot_count = np.round(pd.pivot_table(df, values='Count', 
+                                index='Image Index Time', 
+                                columns='Image Scene Container Name ', 
                                 aggfunc='count'),3)
 
 pivot
-pivot['Time Index'] = df['Time Index'].unique()
-pivot_count['Time Index'] = df['Time Index'].unique()
+pivot['Image Index Time'] = df['Image Index Time'].unique()
+pivot_count['Image Index Time'] = df['Image Index Time'].unique()
 
 del df
 
@@ -77,21 +79,22 @@ well_rows = ["A", "B", "C", "D", "E", "F", "G", "H"]
 
 
 fig, axes = plt.subplots(nrows=8, ncols=12, sharex=True, sharey=True, figsize=(24, 14))
-fig_count, axes = plt.subplots(nrows=8, ncols=12, sharex=True, sharey=True, figsize=(24, 14))
+fig_count, axes_count = plt.subplots(nrows=8, ncols=12, sharex=True, sharey=True, figsize=(24, 14))
 x = 0
 for n in range(len(well_rows)): #well rows
     for m in range(12): #well columns
         well_name.append(well_rows[n] + str(m+1))
         axes[n, m].set(title=well_name[x])
-        pivot.plot(x='Time Index', y=well_name[x], ax=axes[n,m],legend=None)
-        pivot_count.plot(x='Time Index', y=well_name[x], ax=axes[n,m],legend=None)
+        pivot.plot(x='Image Index Time', y=well_name[x], ax=axes[n,m],legend=None)
+        pivot_count.plot(x='Image Index Time', y=well_name[x], ax=axes_count[n,m],legend=None)
         x = x + 1
 
 fig.suptitle('Exp 7 - Dead cells average convexity')
 fig_count.suptitle('Exp 7 - Dead cells count')
 
+fig.savefig('Z:/private/Marie/Image Analysis/2023-08-15-Zen-practice/FD-FD-data/average.png')
+fig_count.savefig('Z:/private/Marie/Image Analysis/2023-08-15-Zen-practice/FD-FD-data/count.png')
 
-"""
 
 print('Done! Deal with it any which way you like. Plotting takes time so be patient!')
 
